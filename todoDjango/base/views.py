d2f7d8bf141af9dcca51abfc397ce2c9b4a4ac86
Rostyk <rostishka1@gmail.com>
2021-06-09 16:01:12 +0300

    forms, style forms, views

--- todoDjango/base/views.py
+++ todoDjango/base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 class TaskList(View):
     def get(self, request):
-        return render(request, 'base/task_list.html')
+        form = TaskForm()
+        tasks = Task.objects.all()
+
+        context = {'form': form, 'tasks': tasks}
+        return render(request, 'base/task_list.html', context)
